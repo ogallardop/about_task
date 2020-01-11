5f89005824bebd6588d2d851cb8c5857f9c80714
Oscar David Gallardo P <oscar.gallardop@gmail.com>
2020-01-11 23:56:46 +0100

    Not Framework configuration

---
+++ www/web/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing;
+
+$request = Request::createFromGlobals();
+$requestStack = new RequestStack();
+$routes = include __DIR__ . '/../app/routes.php';
+
+$context = new Routing\RequestContext();
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+$controllerResolver = new HttpKernel\Controller\ControllerResolver();
+$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
+
+$framework = new HttpKernel\HttpKernel($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+
+$response = $framework->handle($request);
+$response->send();
