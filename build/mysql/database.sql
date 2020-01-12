4db791ffec26b3f56b388374d82c8461ea474628
Oscar David Gallardo P <oscar.gallardop@gmail.com>
2020-01-12 22:49:32 +0100

    database config

--- build/mysql/database.sql
+++ build/mysql/database.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE aboutdb;
+
+CREATE TABLE `aboutdb`.`customer` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `email` VARCHAR(255) NULL,
+  `status` VARCHAR(45) NULL,
+  `country` VARCHAR(255) NULL,
+  `address` VARCHAR(255) NULL,
+  `postal_code` VARCHAR(20) NULL,
+  PRIMARY KEY (`id`));
+
+INSERT INTO `aboutdb`.`customer` (`name`, `email`, `status`, `country`, `address`)
+VALUES ('Oscar', 'oscar.gallardo@outlook.com', 'Registered', 'Germany', 'ups');
